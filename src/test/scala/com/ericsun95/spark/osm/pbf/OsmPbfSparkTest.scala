b75b418c67b8456524a26c37ebdad7463cb32033
ericsun95 <ericsun95@outlook.com>
2020-11-29 23:20:07 -0500

    Update the spark test to speed up.

--- src/test/scala/com/ericsun95/spark/osm/pbf/OsmPbfSparkTest.scala
+++ src/test/scala/com/ericsun95/spark/osm/pbf/OsmPbfSparkTest.scala
@@ -22,16 +22,42 @@ class OsmPbfSparkTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
       .as[OSMInternalRow](osmInternalRowEncoder)
       .cache()
 
-    df.filter(x => x.osmType == 0).show(5)
-    df.filter(x => x.osmType == 1).show(5)
-    df.filter(x => x.osmType == 2).show(5)
-
     assert(df.rdd.getNumPartitions > 1)
     assert(df.rdd.count() == 61217)
-    assert(df.filter(x => x.osmType == 0).count() == 56060)
-    assert(df.filter(x => x.osmType == 1).count() == 5124)
-    assert(df.filter(x => x.osmType == 2).count() == 33)
+
+    val df_nodes = df.filter(x => x.osmType == 0).cache()
+    val df_ways =df.filter(x => x.osmType == 1).cache()
+    val df_relations =df.filter(x => x.osmType == 2).cache()
+
+    df_nodes.show(5)
+    df_ways.show(5)
+    df_relations.show(5)
+
+    assert(df_nodes.count() == 56060)
+    assert(df_ways.count() == 5124)
+    assert(df_relations.count() == 33)
 
     spark.stop()
   }
+
+//  test("OSM PBF Spark DataSource read big data correctly") {
+//    val testFile = "/Users/eric_sun/Desktop/asia-latest.osm.pbf"
+//
+//    val spark = SparkSession
+//      .builder()
+//      .master("local[4]")
+//      .config("spark.executor.memory", "4g")
+//      .config("spark.driver.memory", "4g")
+//      .getOrCreate()
+//
+//    val osmInternalRowEncoder: Encoder[OSMInternalRow] = Encoders.product[OSMInternalRow]
+//    val df = spark.read
+//      .format("com.ericsun95.spark.osm.pbf.DefaultSource")
+//      .load(testFile)
+//      .as[OSMInternalRow](osmInternalRowEncoder)
+//
+//    df.show(10)
+//
+//    spark.stop()
+//  }
 }
